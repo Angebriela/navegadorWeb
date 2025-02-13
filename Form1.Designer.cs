b99d9f503703df344d782f57adfc408886463f93
Angebriela <angelachiyal07@gmail.com>
2025-02-13 02:26:50 -0600

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace navegadorWeb
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.labelNavegador = new System.Windows.Forms.Label();
+            this.textBoxAdress = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.labelbarraSuperior = new System.Windows.Forms.Label();
+            this.buttonBuscar = new System.Windows.Forms.Button();
+            this.webView = new Microsoft.Web.WebView2.WinForms.WebView2();
+            ((System.ComponentModel.ISupportInitialize)(this.webView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labelNavegador
+            // 
+            this.labelNavegador.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelNavegador.AutoSize = true;
+            this.labelNavegador.Font = new System.Drawing.Font("Magical Childhood", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelNavegador.Location = new System.Drawing.Point(12, 39);
+            this.labelNavegador.Name = "labelNavegador";
+            this.labelNavegador.Size = new System.Drawing.Size(211, 39);
+            this.labelNavegador.TabIndex = 0;
+            this.labelNavegador.Text = "NAVEGADOR";
+            this.labelNavegador.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.labelNavegador.UseMnemonic = false;
+            // 
+            // textBoxAdress
+            // 
+            this.textBoxAdress.Location = new System.Drawing.Point(12, 91);
+            this.textBoxAdress.Name = "textBoxAdress";
+            this.textBoxAdress.Size = new System.Drawing.Size(651, 20);
+            this.textBoxAdress.TabIndex = 1;
+            this.textBoxAdress.TextChanged += new System.EventHandler(this.textBoxAdress_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AccessibleName = "";
+            this.label1.Image = ((System.Drawing.Image)(resources.GetObject("label1.Image")));
+            this.label1.Location = new System.Drawing.Point(219, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 69);
+            this.label1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AccessibleName = "";
+            this.label2.BackColor = System.Drawing.Color.SkyBlue;
+            this.label2.Location = new System.Drawing.Point(0, 401);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(898, 33);
+            this.label2.TabIndex = 3;
+            this.label2.Visible = false;
+            // 
+            // labelbarraSuperior
+            // 
+            this.labelbarraSuperior.AccessibleName = "";
+            this.labelbarraSuperior.BackColor = System.Drawing.Color.SkyBlue;
+            this.labelbarraSuperior.Location = new System.Drawing.Point(0, -5);
+            this.labelbarraSuperior.Name = "labelbarraSuperior";
+            this.labelbarraSuperior.Size = new System.Drawing.Size(898, 33);
+            this.labelbarraSuperior.TabIndex = 4;
+            // 
+            // buttonBuscar
+            // 
+            this.buttonBuscar.Location = new System.Drawing.Point(669, 88);
+            this.buttonBuscar.Name = "buttonBuscar";
+            this.buttonBuscar.Size = new System.Drawing.Size(106, 23);
+            this.buttonBuscar.TabIndex = 5;
+            this.buttonBuscar.Text = "Buscar 🔍";
+            this.buttonBuscar.UseVisualStyleBackColor = true;
+            this.buttonBuscar.Click += new System.EventHandler(this.buttonBuscar_Click);
+            // 
+            // webView
+            // 
+            this.webView.AllowExternalDrop = true;
+            this.webView.CreationProperties = null;
+            this.webView.DefaultBackgroundColor = System.Drawing.Color.White;
+            this.webView.Location = new System.Drawing.Point(-7, 120);
+            this.webView.Name = "webView";
+            this.webView.Size = new System.Drawing.Size(826, 314);
+            this.webView.Source = new System.Uri("https://www.microsoft.com", System.UriKind.Absolute);
+            this.webView.TabIndex = 6;
+            this.webView.ZoomFactor = 1D;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(810, 428);
+            this.Controls.Add(this.textBoxAdress);
+            this.Controls.Add(this.buttonBuscar);
+            this.Controls.Add(this.labelbarraSuperior);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.labelNavegador);
+            this.Controls.Add(this.webView);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.webView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Microsoft.Web.WebView2.WinForms.WebView2 webView;
+        private System.Windows.Forms.Label labelNavegador;
+        private System.Windows.Forms.TextBox textBoxAdress;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label labelbarraSuperior;
+        private System.Windows.Forms.Button buttonBuscar;
+        
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.Web.WebView2.Core;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace navegadorWeb
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.Resize += new System.EventHandler(this.Form_Resize);
+            webView.NavigationStarting += EnsureHttps;
+            InitializeAsync();
+
+        }
+
+        private void Form_Resize(object sender, EventArgs e)
+        {
+            webView.Size = this.ClientSize - new System.Drawing.Size(webView.Location);
+            buttonBuscar.Left = this.ClientSize.Width - buttonBuscar.Width;
+            textBoxAdress.Width = buttonBuscar.Left - textBoxAdress.Left;
+            labelbarraSuperior.Size = this.ClientSize = new System.Drawing.Size(labelbarraSuperior.Location);
+            labelNavegador.Left= this.ClientSize.Width;
+        }
+
+        void EnsureHttps(object sender, CoreWebView2NavigationStartingEventArgs args)
+        {
+            String uri = args.Uri;
+            if (!uri.StartsWith("https://"))
+            {
+                webView.CoreWebView2.ExecuteScriptAsync($"alert('{uri} is not safe, try an https link')");
+                args.Cancel = true;
+            }
+        }
+
+        async void InitializeAsync()
+        {
+            await webView.EnsureCoreWebView2Async(null);
+            webView.CoreWebView2.WebMessageReceived += UpdateAddressBar;
+            await webView.CoreWebView2.AddScriptToExecuteOnDocumentCreatedAsync("window.chrome.webview.postMessage(window.document.URL);");
+            await webView.CoreWebView2.AddScriptToExecuteOnDocumentCreatedAsync("window.chrome.webview.addEventListener(\'message\', event => alert(event.data));");
+
+        }
+
+        void UpdateAddressBar(object sender, CoreWebView2WebMessageReceivedEventArgs args)
+        {
+            String uri = args.TryGetWebMessageAsString();
+            textBoxAdress.Text = uri;
+            webView.CoreWebView2.PostWebMessageAsString(uri);
+        }
+
+        private void buttonBuscar_Click(object sender, EventArgs e)
+        {
+            string adress = textBoxAdress.Text;
+
+            if (!adress.StartsWith("https:/"))
+            {
+                textBoxAdress.Text = "https://" + adress;
+            }
+            
+            if (!adress.EndsWith(".com"))
+            {
+                textBoxAdress.Text = "https://www.google.com/search?q=" + adress;
+
+            }
+
+            if (webView != null && webView.CoreWebView2 != null)
+            {
+                webView.CoreWebView2.Navigate(textBoxAdress.Text);
+            }
+
+            
+
+            
+
+        }
+
+        private void textBoxAdress_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
